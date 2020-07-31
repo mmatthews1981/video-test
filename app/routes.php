66def9d656fbba31a2722282539811122ff5a924
Meredith Matthews <mmatthews@mtb.com>
2020-07-31 15:14:58 -0400

    added new endpoint to backend

--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 use App\Application\Actions\User\ListUsersAction;
 use App\Application\Actions\User\ViewUserAction;
+use App\Application\Actions\Video\ListVideosAction;
+use App\Application\Actions\Video\ViewVideoAction;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
@@ -22,5 +24,10 @@ return function (App $app) {
     $app->group('/users', function (Group $group) {
         $group->get('', ListUsersAction::class);
         $group->get('/{id}', ViewUserAction::class);
+    });
+
+    $app->group('/videos', function (Group $group) {
+        $group->get('', ListVideosAction::class);
+        $group->get('/{id}', ViewVideoAction::class);
     });
 };
