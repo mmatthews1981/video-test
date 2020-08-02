5a5d6642b8150d36feb41b7704e36ae01aae4fd1
Meredith Matthews <mmatthews@mtb.com>
2020-08-02 00:05:18 -0400

    added video filter

--- frontend/pages/videos/index.vue
+++ frontend/pages/videos/index.vue
@@ -6,13 +6,19 @@
           <h1>Welcome to Our Video Library</h1>
         </b-col>
       </b-row>
+      <b-row>
+        <b-col col="3">
+          <b-form-select v-model="selected" :options="options"></b-form-select>
+        </b-col>
+      </b-row>
       <b-row class="summaries">
             <VideoSummary v-for="video in videos" 
               v-bind:key="video.id"
               v-bind:title="video.title"
               v-bind:description="video.description" 
               v-bind:id="video.id"
-              v-bind:video_id="video.video_id" />
+              v-bind:video_id="video.video_id" 
+              v-if="categoryfilter(video.category)"/>
       </b-row>
     </b-container>
   </div>
@@ -25,13 +31,23 @@ export default {
   data() {
     return {
       videos: { },
-      test: {}
+      test: {},
+      selected: null,
+      options: [{ value: null, text: 'Filter by category' },
+                { value: "Testimonials", text: "Testimonials"}
+              ]
     };
+  },
+  computed:{
+    
   },
   methods: {
     async getVideos() {
       let res = await this.$axios.get('http://localhost:8080/videos')
       this.videos = res.data.data;
+    },
+    categoryfilter(cat){
+      return this.selected === null || this.selected === cat
     }
   },
   mounted() {
@@ -45,6 +61,11 @@ export default {
 
 .container-fluid{
   margin: 50px 0;
+}
+
+select {
+  max-width: 400px;
+  margin: 30px 0;
 }
 
 .summaries {
